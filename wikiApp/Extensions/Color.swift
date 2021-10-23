66cb0c8222c683abb35263d419f2774b5fec2ebe
tiking76 <716yosiki1999@gmail.com>
2021-10-24 00:06:02 +0900

    add extension

---
+++ wikiApp/Extensions/Color.swift
@@ -0,0 +1,12 @@
+import Foundation
+import SwiftUI
+
+// Todo: fix work Color
+extension UIColor {
+    static let lightOrange = UIColor(red: 255/255, green: 249/255, blue: 249/255, alpha: 1)
+    static let imageBorder = UIColor(red: 196 / 255, green: 196 / 255, blue: 196 / 255, alpha: 0.5)
+}
+
+extension Color {
+    static let imageBorder = Color(UIColor.imageBorder)
+}
