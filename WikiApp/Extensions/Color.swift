85fd454f62867aa323e830ce7f43f9407c26574c
tiking76 <716yosiki1999@gmail.com>
2021-10-26 22:41:41 +0900

    wip create login view

--- WikiApp/Extensions/Color.swift
+++ WikiApp/Extensions/Color.swift
@@ -3,10 +3,13 @@ import SwiftUI
 
 // Todo: fix work Color
 extension UIColor {
-    static let lightOrange = UIColor(red: 255/255, green: 249/255, blue: 249/255, alpha: 1)
-    static let imageBorder = UIColor(red: 196 / 255, green: 196 / 255, blue: 196 / 255, alpha: 0.5)
+    static let themeColor = UIColor(red: 255/255, green: 249/255, blue: 249/255, alpha: 1)
+    static let background = UIColor(red: 255 / 255, green: 252 / 255, blue: 248 / 255, alpha: 1)
+    static let primary = UIColor(red: 247 / 255, green: 165 / 255, blue: 17 / 255, alpha: 1)
 }
 
 extension Color {
-    static let imageBorder = Color(UIColor.imageBorder)
+    static let themeColor = Color(UIColor.themeColor)
+    static let background = Color(UIColor.background)
+    static let primary = Color(UIColor.primary)
 }
---
+++ WikiApp/Login/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  WikiApp
+//
+//  Created by tiking on 2021/10/26.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Color.background.ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 0) {
+                
+            }
+        }
+    }
+}
