7f551abaa08492bbbe1570895f647f07592d2bc5
Kojima113 <s1270113@u-aizu.ac.jp>
2021-10-26 22:52:41 +0900

    wip create view

---
+++ WikiApp/PostArticle/PostArticleView.swift
@@ -0,0 +1,14 @@
+//
+//  PostArticleView.swift
+//  WikiApp
+//
+//  Created by 小島大輝 on 2021/10/26.
+//
+
+import SwiftUI
+
+struct PostArtticleView: View {
+    var body: some View {
+        Text("test")
+    }
+}
