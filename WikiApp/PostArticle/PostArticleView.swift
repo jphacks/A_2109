666182911485959a6027adcb4616fdcf54169332
Kojima113 <s1270113@u-aizu.ac.jp>
2021-10-27 16:37:52 +0900

    記事内容の先頭の位置を調整

--- WikiApp/PostArticle/PostArticleView.swift
+++ WikiApp/PostArticle/PostArticleView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct PostArtticleView: View {
     @State private var name = "";var body: some View {NavigationView{
             
-                TextField("記事内容をここに投稿", text: $name).frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .topLeading).navigationTitle("記事投稿").navigationBarTitleDisplayMode(.inline)
+        TextField("記事内容をここに投稿", text: $name).frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .topLeading).navigationTitle("記事投稿").navigationBarTitleDisplayMode(.inline).padding(.leading, 16)
             
                 .toolbar{
                     ToolbarItem(placement: .navigationBarTrailing){
