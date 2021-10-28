5e4a4d044f8146b2719dc3bcd15087daf55c7421
Kojima113 <s1270113@u-aizu.ac.jp>
2021-10-28 19:03:22 +0900

    modifierごとに改行

--- WikiApp/PostArticle/PostArticleView.swift
+++ WikiApp/PostArticle/PostArticleView.swift
@@ -9,18 +9,22 @@ import SwiftUI
 
 struct PostArtticleView: View {
     @State private var name = "";var body: some View {NavigationView{
-            
-        TextField("記事内容をここに投稿", text: $name).frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .topLeading).navigationTitle("記事投稿").navigationBarTitleDisplayMode(.inline).padding(.leading, 16)
-            
-                .toolbar{
-                    ToolbarItem(placement: .navigationBarTrailing){
-                                        Button("投稿") {}
-                                    }
-                    ToolbarItem(placement: .navigationBarLeading){
-                        Button("戻る"){}
-                    }
+        
+        TextField("記事内容をここに投稿", text: $name)
+            .frame(maxWidth: .infinity,maxHeight:.infinity, alignment:.topLeading)
+            .navigationTitle("記事投稿")
+            .navigationBarTitleDisplayMode(.inline)
+            .padding(.leading, 16)
+        
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button("投稿") {}
                 }
-        }
+                ToolbarItem(placement: .navigationBarLeading){
+                    Button("戻る"){}
+                }
+            }
+    }
         
     }
 }
